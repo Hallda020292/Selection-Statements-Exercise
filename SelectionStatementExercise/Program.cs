7b85579f63ddccb132453324e5e5f8b853258f51
Dakota hall <Hallda020292@gmail.com>
2022-11-04 09:43:52 -0400

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int FavNumber = 5;
+            Console.WriteLine("try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput > FavNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            if (userInput < FavNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput == FavNumber)
+            {
+                Console.WriteLine("nevermind you got it correct!");
+            }
+                }
+
         }
     }
-}
